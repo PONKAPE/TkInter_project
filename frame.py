aa4b686e14faf963bfacab1b3713c20ae0c783fd
ponkape <ponkanen@gmail.com>
2018-08-27 20:27:42 +0300

    Added dropdown with About and Exit

--- frame.py
+++ frame.py
@@ -1,11 +1,11 @@
-import tkinter as tk # Python 3
+from tkinter import *
 from PIL import ImageTk, Image
 
 # Main window
-window = tk.Tk()
-window.title("NHL 94")
-window.geometry("700x400")
-window.configure(background='grey')
+root = Tk()
+root.title("NHL 94")
+root.geometry("700x400")
+root.configure(background='grey')
 
 path = "nhl.jpg"
 
@@ -13,10 +13,25 @@ path = "nhl.jpg"
 img = ImageTk.PhotoImage(Image.open(path))
 
 # Widget 
-panel = tk.Label(window, image = img)
+panel = Label(root, image = img)
 
 # Put widget inside a frame with .pack()
 panel.pack(side = "bottom", fill = "both", expand = "yes")
 
 
-window.mainloop()
+# Drop down Menu
+
+def exit_program():
+    sys.exit()
+
+menu = Menu(root)
+root.config(menu=menu)
+
+subMenu = Menu(menu)
+menu.add_cascade(label="File", menu=subMenu) 
+subMenu.add_command(label="About", command=exit_program) # About section..
+subMenu.add_separator()
+subMenu.add_command(label="Exit", command=exit_program)
+
+
+root.mainloop()
