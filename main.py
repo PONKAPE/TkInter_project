41bcff5e979eaa7ec2da2af26b08f840d641e134
ponkape <ponkanen@gmail.com>
2019-01-02 22:07:17 +0200

    Prework for sounds

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
 import tkinter as tk
 from PIL import ImageTk, Image
 import sys, os
 import random
+import mp3play
 
 class MainWindow(tk.Frame):
 
@@ -19,7 +21,7 @@ class MainWindow(tk.Frame):
         menubar = MenuBar(self)
         root.configure(menu=menubar)
         # Title, font & window size
-        root.title("NHL 94") 
+        root.title("NHL 94")
         root.option_add('*font', ('verdana', 16, 'bold'))
         root.geometry("700x450")
         root. resizable(0, 0) # Don't allow resizing
@@ -29,8 +31,11 @@ class MainWindow(tk.Frame):
         img = tk.Label(self, image=render)
         img.image = render
         img.place(x=0, y=0)
+        # Mp3 theme
+        mp3 = mp3play.load('theme.mp3'); play = lambda: mp3.play()
 
-    def new_window(self): 
+
+    def new_window(self):
         self.newWindow = tk.Toplevel(self.parent)
         self.main = random_team(self.newWindow)
 
@@ -57,8 +62,8 @@ class MenuBar(tk.Menu):
         popup.wm_title("About")
         label = tk.Label(popup, text=msg)
         label.pack(fill="x", pady=10)
-    
-    
+
+
 
 class team_window(tk.Frame):
     def __init__(self, parent):
@@ -66,15 +71,15 @@ class team_window(tk.Frame):
         # Frame configurations
         parent.minsize(width=999, height=500)
         parent.title("NHL Teams")
-     
+
         ### Teams-section. Explanation for next solution for showing team logos
         ### http://effbot.org/pyfaq/why-do-my-tkinter-images-not-appear.htm
-        ### 
-        ### "The problem is that the Tkinter/Tk interface doesn’t handle references to Image objects properly; the Tk widget will hold a reference to the internal object, but Tkinter does not. 
+        ###
+        ### "The problem is that the Tkinter/Tk interface doesn’t handle references to Image objects properly; the Tk widget will hold a reference to the internal object, but Tkinter does not.
         ### When Python’s garbage collector discards the Tkinter object, Tkinter tells Tk to release the image.
-        ### But since the image is in use by a widget, Tk doesn’t destroy it. Not completely. 
+        ### But since the image is in use by a widget, Tk doesn’t destroy it. Not completely.
         ### It just blanks the image, making it completely transparent…"
-        ### 
+        ###
         ###
 
         text_team = tk.Label(parent, text="HALL OF FAME", fg="orange", anchor="n")
@@ -95,7 +100,7 @@ class Team():
     def __init__(self, name, img_filename):
         self.name = name
         self.img_filename = img_filename
-        
+
     @property
     def filepath(self):
         """
@@ -109,15 +114,15 @@ class Team():
         :return: A string describe this Team data
         """
         return "Name: {} Image:{}".format(self.name, self.img_filename)
-    
-    
+
+
 class TeamWidget(tk.Label):
     """
     Inherit from tk.Label to build a Team specific Label showing Image and Text
     """
     def __init__(self, parent, team, side):
         """
-        :param parent: The Parent where this Widget depends on 
+        :param parent: The Parent where this Widget depends on
         :param team: A class Team object
         :param side: How to pack this Label to (left|right)
         """
@@ -132,7 +137,7 @@ class TeamWidget(tk.Label):
     @property
     def team(self):
         """
-        :return: The class Team object of this TeamWidget 
+        :return: The class Team object of this TeamWidget
         """
         return self._team
 
@@ -140,11 +145,11 @@ class TeamWidget(tk.Label):
     def team(self, team):
         """
         NOTE: This works ONLY for PNG Images!
-        
-        Setting a class Team object, read its Logo Image and 
+
+        Setting a class Team object, read its Logo Image and
         configure the Lable Widget with text and image
-        
-        :param team: The class Team object related to this TeamWidget 
+
+        :param team: The class Team object related to this TeamWidget
         :return: None
         """
         self._team = team
@@ -159,9 +164,9 @@ class random_team(tk.Frame):
         parent.title("RANDOMIZE")
         # TEXT
         text_1 = tk.Label(parent, text="VS", fg="red", anchor="center")
-        text_1.place(relx=.5, rely=.5, anchor="center") 
+        text_1.place(relx=.5, rely=.5, anchor="center")
         text_2 = tk.Label(parent, text="RANDOM GAME", fg="green", anchor="n")
-        text_2.pack(side="top") 
+        text_2.pack(side="top")
         # Button for randomize
         self.parent = parent
         self.frame = tk.Frame(self.parent)
@@ -179,7 +184,7 @@ class random_team(tk.Frame):
         home_label = tk.Label(parent, textvariable = self.home)
         home_label.pack(side="left")
         # Visitor team
-        load = Image.open("project/images/question.jpg") # https://www.charbase.com/fe56-unicode-small-question-mark 
+        load = Image.open("project/images/question.jpg") # https://www.charbase.com/fe56-unicode-small-question-mark
         render = ImageTk.PhotoImage(load)
         visitor_img = tk.Label(parent, image=render)
         visitor_img.image = render
@@ -193,9 +198,9 @@ class random_team(tk.Frame):
         # Init defaults
         self.home = self.teams[0]
         self.visitor = self.teams[0]
-        
-        
-    def genRanTeam(self): 
+
+
+    def genRanTeam(self):
         self.__init__Teams()  # Use the initalized Teams from MainWindow..__init__
         self.home = random.choice(self.teams)
         self. visitor = None
@@ -203,7 +208,7 @@ class random_team(tk.Frame):
         while self.visitor is None or self.home.name is self.visitor.name:
          self.visitor = random.choice(self.teams)
         return
-        
+
 
 if __name__ == "__main__":
     root = tk.Tk()
