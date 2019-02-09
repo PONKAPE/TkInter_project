f2aa685f770216a0cf3f2efc977e218d8ebc2123
ponkape <ponkanen@gmail.com>
2019-02-09 11:44:06 +0200

    Logos edited to .gif, becacause tkinter has problems reading .jpg in random section

--- main.py
+++ main.py
@@ -105,25 +105,17 @@ class Team():
         """
         :return: The filepath of this Team Logo image file.
         """
-        return self.img_filename
-
-    def __str__(self):
-        """
-        This is not used in this example, use it for debuging Name - Logo image filename missmatch
-        :return: A string describe this Team data
-        """
-        return "Name: {} Image:{}".format(self.name, self.img_filename)
-
+        _map = {'New York Islanders': 'ny_islanders.gif','Montreal Canadiens': 'montreal.gif','Los Angeles Kings': 'los_angeles.gif','Hartford Whalers': 'hartford.gif','Calgary Flames': 'calgary.gif','Buffalo Sabres': 'buffalo.gif','Anaheim Mighty Ducks': 'anaheim.gif','Boston Bruins': 'Boston.gif','Chicago Blackhawks': 'chicago.gif', 'New York Rangers': 'ny_rangers.gif',
+                'All-Stars West': 'west.gif','Pittsburgh Penguins': 'pittsburgh.gif','Ottawa Senators': 'ottawa.gif','Florida Panthers': 'florida.gif','Edmonton Oilers': 'edmonton.gif','All-Stars East': 'east.gif','New Jersey Devils': 'devils.gif','Detroit Red Wings': 'detroit.gif','Dallas Stars': 'dallas.gif','Philadelphia Flyers': 'philadelphia.gif', 'Toronto Maple Leafs': 'toronto.gif',
+                'Vancouver Canucks': 'vancouver.gif','Winnipeg Jets': 'winnipeg.gif','Washington Capitals': 'washington.gif','Tampa Bay Lightning': 'tampa_bay.gif','St. Louis Blues': 'st_louis.gif','San Jose Sharks': 'sharks.gif','Quebec Nordiques': 'quebec.gif'}
+        return os.path.join('project\images', _map[self.name])
 
 class TeamWidget(tk.Label):
-    """
-    Inherit from tk.Label to build a Team specific Label showing Image and Text
-    """
     def __init__(self, parent, team, side):
         """
         :param parent: The Parent where this Widget depends on
         :param team: A class Team object
-        :param side: How to pack this Label to (left|right)
+        :p aram side: How to pack this Label to (left|right)
         """
         self._team = None
         super().__init__(parent, Image=None, text=team.name,
@@ -142,15 +134,10 @@ class TeamWidget(tk.Label):
 
     @team.setter
     def team(self, team):
-        """
-        NOTE: This works ONLY for PNG Images!
 
-        Setting a class Team object, read its Logo Image and
-        configure the Lable Widget with text and image
+        # Setting a class Team object, read its Logo Image and
+        # Configure the Lable Widget with text and image
 
-        :param team: The class Team object related to this TeamWidget
-        :return: None
-        """
         self._team = team
         # IMPORTANT: Keep Image reference to not get garbage collected
         self.image = tk.PhotoImage(file=team.filepath)
@@ -174,39 +161,56 @@ class random_team(tk.Frame):
         self.frame.pack(side="bottom")
         self.home = tk.StringVar()
         self.visitor = tk.StringVar()
+
+        # INIT
+        self.__init__Teams()
+
         # Home team
-        load = Image.open("project/images/question.jpg") # https://www.charbase.com/fe56-unicode-small-question-mark
-        render = ImageTk.PhotoImage(load)
-        home_img = tk.Label(parent, image=render)
-        home_img.image = render
-        home_img.pack(side="left")
-        home_label = tk.Label(parent, textvariable = self.home)
-        home_label.pack(side="left")
+        self.home_label = TeamWidget(parent, Team('Team Home', None), 'left', )
         # Visitor team
-        load = Image.open("project/images/question.jpg") # https://www.charbase.com/fe56-unicode-small-question-mark
-        render = ImageTk.PhotoImage(load)
-        visitor_img = tk.Label(parent, image=render)
-        visitor_img.image = render
-        visitor_img.pack(side="right")
-        visitor_label = tk.Label(parent, textvariable = self.visitor)
-        visitor_label.pack(side="right")
+        self.visitor_label = TeamWidget(parent, Team('Team Visitor', None), 'right')
 
 
+        # Init teams
     def __init__Teams(self):
-        self.teams = [Team('Allstars East', east.jpg), Team('Allstars West', west.jpg), Team('Anaheim', anaheim.jpg), Team('Boston', boston.jpg), Team('Buffalo', buffalo.jpg), Team('Calgary', calgary.jpg), Team('Chicago', chicago.jpg), Team('Dallas', dallas.jpg), Team('Detroit', detroit.jpg), Team('Edmonton', edmonton.jpg), Team('Florida', florida.jpg),  Team('Hartford', hartford.jpg), Team('Los Angeles', los_angeles.jpg), Team('Montreal', montreal.jpg), Team('NY Islanders', ny_islanders.jpg), Team('NY Rangers', ny_rangers.jpg), Team('Ottawa', ottawa.jpg), Team('Philadelphia', philadelphia.jpg), Team('Pittsburgh', pittsburgh.jpg), Team('Quebec', quebec.jpg), Team('San Jose', sharks.jpg), Team('St. Louis', st_louis.jpg), Team('Tampa Bay', tampa_bay.jpg), Team('Toronto', toronto.jpg), Team('Vancouver', vancouver.jpg), Team('Washington', washington.jpg), Team('Winnipeg', winnipeg.jpg)]
-        # Init defaults
-        self.home = self.teams[0]
-        self.visitor = self.teams[0]
+        self.teams = [Team('Anaheim Mighty Ducks', 'anaheim.jpg'),
+                      Team('Chicago Blackhawks', 'chicago.gif'),
+                      Team('Philadelphia Flyers', 'philadelphia.gif'),
+                      Team('Boston Bruins', 'boston.gif'),
+                      Team('Calgary Flames', 'calgary.gif'),
+                      Team('Dallas Stars', 'dallas.gif'),
+                      Team('Detroit Red Wings', 'detroit.gif'),
+                      Team('New Jersey Devils', 'Devils.gif'),
+                      Team('All-Stars East', 'east.gif'),
+                      Team('All-Stars West', 'west.gif'),
+                      Team('Edmonton Oilers', 'edmonton.gif'),
+                      Team('Florida Panthers', 'florida.gif'),
+                      Team('Hartford Whalers', 'hartford.gif'),
+                      Team('Los Angeles Kings', 'los_angeles.gif'),
+                      Team('Montreal Canadiens', 'montreal.gif'),
+                      Team('New York Islanders', 'ny_islanders.gif'),
+                      Team('New York Rangers', 'ny_rangers.gif'),
+                      Team('Ottawa Senators', 'ottawa.gif'),
+                      Team('Pittsburgh Penguins', 'pittsburgh.gif'),
+                      Team('Quebec Nordiques', 'quebec.gif'),
+                      Team('San Jose Sharks', 'sharks.gif'),
+                      Team('St. Louis Blues', 'st_louis.gif'),
+                      Team('Tampa Bay Lightning', 'tampa_bay.gif'),
+                      Team('Vancouver Canucks', 'vancouver.gif'),
+                      Team('Washington Capitals', 'washington.gif'),
+                      Team('Buffalo Sabres', 'buffalo.gif'),
+                      Team('Winnipeg Jets', 'winnipeg.gif'),
+                      Team('Toronto Maple Leafs', 'toronto.jpg')]
 
 
     def genRanTeam(self):
-        self.__init__Teams()  # Use the initalized Teams from MainWindow..__init__
-        self.home = random.choice(self.teams)
-        self. visitor = None
+        # Use the initalized Teams from __init__
+        self.home_label.team = random.choice(self.teams)
+        visitor = None
         # Loop while home == visitor
-        while self.visitor is None or self.home.name is self.visitor.name:
-         self.visitor = random.choice(self.teams)
-        return
+        while visitor is None or self.home_label.team is visitor:
+            visitor = random.choice(self.teams)
+        self.visitor_label.team = visitor
 
 
 if __name__ == "__main__":
